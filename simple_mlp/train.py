8f89a4f5c51e1a1ee42385dac49562d911d5b50c
Daniel Derkacz <mail@daniel-derkacz.de>
2021-05-25 18:17:43 +0200

    somehow the mlp will not overfit

--- simple_mlp/train.py
+++ simple_mlp/train.py
@@ -1,25 +1,54 @@
 import torch
 from torch import nn
+import torch.nn.functional as F
 import numpy as np
 import random
 from tensorboardX import SummaryWriter
+import argparse
+import pickle
+import matplotlib.pyplot as plt
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--eval", type=str, default="")
+parser.add_argument("--name", type=str)
+parser.add_argument("--epochs", type=int, default=10000)
+parser.add_argument("--neurons", type=int, default=20)
+parser.add_argument("--lr", type=float, default=1e-3)
+parser.add_argument("--min", type=int, default=1)
+parser.add_argument("--max", type=int, default=50)
+parser.add_argument("--layers", type=int, default=3)
+parser.add_argument("--batch", type=int, default=32)
+parser.add_argument("--normalized", type=str, default="no")
+args = parser.parse_args()
+
+normalized = True if args.normalized == "yes" else False
+
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 class SimpleMLP(nn.Module):
     def __init__(self):
         super(SimpleMLP, self).__init__()
-        self.mlp_stack = nn.Sequential(
-            nn.Linear(2, 5),
-            nn.ReLU(),
-            nn.Linear(5, 5),
-            nn.ReLU(),
-            nn.Linear(5, 5),
-            nn.ReLU(),
-            nn.Linear(5, 2),
-            nn.Softmax()
-        )
+
+        # self.mlp_stack = nn.Sequential(
+        #     nn.Linear(2, args.neurons),
+        #     nn.ReLU(),
+        #     nn.Linear(args.neurons, 2),
+        #     nn.Softmax()
+        # )
+
+        self.mlp_stack = nn.ModuleList([nn.Linear(2, args.neurons)])
+        self.mlp_stack.extend([nn.Linear(args.neurons, args.neurons) for i in range(args.layers-2)])
+        self.mlp_stack.append(nn.Linear(args.neurons, 2))
 
     def forward(self, x):
-        return self.mlp_stack(x)
+        #return self.mlp_stack(x)
+        result = x
+        for i in range(args.layers-1):
+            result = F.relu(self.mlp_stack[i](result))
+        return F.softmax(self.mlp_stack[-1](result))
+
 
 class DataGenerator():
     def __init__(self):
@@ -28,22 +57,38 @@ class DataGenerator():
     def generate(self, min, max):
         one, two = float(random.randint(min, max)), float(random.randint(min, max))
         total = one + two
-        one_n, two_n = float(one / total), float(two / total)
-        return torch.tensor([one, two], dtype=torch.float32)
+        #one_n, two_n = float(one / total), float(two / total)
+        return torch.tensor([one, two], dtype=torch.float)
+
+    def generate_choice(self, min, max):
+        range = max - min
+        diff = range / 5
+        values = [min, min+diff, min+2*diff, min+3*diff, min+4*diff]
+        return torch.tensor([np.random.choice(values), np.random.choice(values)], dtype=torch.float)
+
+    def generate_without_doubles(self, min, max, normalized):
+        one, two = float(random.randint(min, max)), float(random.randint(min, max))
+        while one == two:
+            two = float(random.randint(min, max))
+        if normalized:
+            total = one + two
+            one, two = float(one/total), float(two/total)
+        return torch.tensor([one, two], dtype=torch.float)
+        
 
     def batch(self, batch_size, min, max):
         batch = torch.zeros((batch_size, 2), dtype=torch.float)
         labels = torch.zeros(batch_size, dtype=torch.long)
         for i in range(batch_size):
-            batch[i] = self.generate(min, max)
+            batch[i] = self.generate_without_doubles(min, max, normalized)
             labels[i] = np.argmax(batch[i])
-        return batch, labels
+        return batch.to(DEVICE), labels.to(DEVICE)
 
 
 def train(datagen, model, loss_fn, optimizer, batch_size):
     #data = datagen.generate()
-    batch, labels = datagen.batch(batch_size, 10, 100)
-    preds = torch.zeros((batch_size, 2))
+    batch, labels = datagen.batch(batch_size, args.min, args.max)
+    preds = torch.zeros((batch_size, 2)).to(DEVICE)
     for i in range(batch_size):
         preds[i] = model(batch[i])
     loss = loss_fn(preds, labels)
@@ -52,39 +97,81 @@ def train(datagen, model, loss_fn, optimizer, batch_size):
     loss.backward()
     optimizer.step()
 
-    print(f"loss: {loss.item():>7f}")
     return loss.item()
 
 def test(datagen, model, loss_fn, val_size):
     model.eval()
     test_loss, correct = 0, 0
     with torch.no_grad():
-        preds = torch.zeros((val_size, 2), dtype=torch.float)
-        batch, labels = datagen.batch(val_size, 110, 200)
+        preds = torch.zeros((val_size, 2), dtype=torch.float).to(DEVICE)
+        batch, labels = datagen.batch(val_size, args.min + args.max, args.max * 2)
         for i in range(val_size):
             preds[i] = model(batch[i])
         test_loss = loss_fn(preds, labels).item()
         correct += (preds.argmax(1) == labels).type(torch.float).sum().item()
     # test_loss /= val_size
     # correct /= val_size
-    print(f"val loss: {test_loss:>7f}")
 
     return test_loss, correct
 
+def dump_parameters():
+    with open(f"runs/{args.name}/params.txt", "w") as f:
+        f.write(f"Learning rate {args.lr}\n")
+        f.write(f"Num Neurons {args.neurons}\n")
+        f.write(f"Epochs {args.epochs}\n")
+        f.write(f"Min Range {args.min}\n")
+        f.write(f"Max Range {args.max}\n")
+        f.write(f"Batch size {args.batch}\n")
+
+def save_figure(train_loss, val_loss):
+    fig = plt.figure()
+    axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+    axes.plot(np.arange(0, args.epochs), train_loss, label='train loss')
+    axes.plot(np.arange(0, args.epochs), val_loss, label='validation loss')
+    fig.legend()
+    axes.set_xlabel("epochs")
+    axes.set_ylabel("loss")
+    fig.savefig(f"runs/{args.name}/graph.png")
+    plt.close()
+
+
 model = SimpleMLP()
+model.to(DEVICE)
 datagen = DataGenerator()
 loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 
-writer = SummaryWriter()
+writer = SummaryWriter(f"runs/{args.name}")
+dump_parameters()
+train_loss, test_loss = [], []
 
+if args.eval == "":
 
+    for t in range(args.epochs):
+        loss = train(datagen, model, loss_fn, optimizer, args.batch)
+        tloss, correct = test(datagen, model, loss_fn, 100)
+        print(f"Epoch {t}\n-----------------")
+        print(f"loss: {loss:>7f}")
+        print(f"val loss: {tloss:>7f}")
+        print(f"DEVICE {DEVICE}")
+        writer.add_scalar('loss', loss, t)
+        writer.add_scalar('test_loss', tloss, t)
+        writer.add_scalar('correct', 100*correct, t)
+        train_loss.append(loss)
+        test_loss.append(tloss)
 
-for t in range(10000000):
-    print(f"Epoch {t}\n-----------------")
-    loss = train(datagen, model, loss_fn, optimizer, 32)
-    test_loss, correct = test(datagen, model, loss_fn, 100)
-    writer.add_scalar('loss', loss, t)
-    writer.add_scalar('test_loss', test_loss, t)
-    writer.add_scalar('correct', 100*correct, t)
+    torch.save(model.state_dict(), f"runs/{args.name}/model.pth")
+    print(len(train_loss))
+    save_figure(train_loss, test_loss)
+    with open(f"runs/{args.name}/data.pkl", "wb") as f:
+        pickle.dump({"train_loss": train_loss, "test_loss": test_loss}, f)
+    print(f"saved model to runs/{args.name}/model.pth")
 
+else:
+    model.load_state_dict(torch.load("model.pth"))
+    while True:
+        tmp_input = input('some input:')
+        values = tmp_input.split(',')
+        x = torch.tensor([float(values[0]), float(values[1])], dtype=torch.float)
+        pred = model(x)
+        print(f"the prediction is {pred}")
