1ee4d8f20cb961fadc580815e70de36566be1182
Daniel Derkacz <mail@daniel-derkacz.de>
2021-08-03 21:09:45 +0200

    custom gen8randombattle with only 3 pokemon (not finished yet need to switch to different showdown commit)

--- create_datacollection_pipeline.sh
+++ create_datacollection_pipeline.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/bash
 
-count=16
+count=1
 dest="pmariglia/envs"
 websocket="showdown-server"
 service="showdown"
 port=8081
 baseip="172.25.0.0"
-gamemode="gen8custombattle"
+# Modes:
+# gen8customgame
+# gen8randombattle
+# gen8randombattle3  (not finished yet, need to switch showdown to newer commit)
+gamemode="gen8customgame"
 
 python3 pmariglia/envs/create_envs.py --websocket "$websocket:$port" --count $count --runcount 10 --gamemode "$gamemode@@@Dynamax Clause" --datadir dataset --battlebot most_damage --dest $dest 
 
