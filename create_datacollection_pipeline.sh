3132b4954353bb5ad16f31af49197934f710817e
Daniel Derkacz <mail@daniel-derkacz.de>
2021-08-04 11:55:24 +0200

    Added teamdir and baseip to data collection pipeline shell script.

--- create_datacollection_pipeline.sh
+++ create_datacollection_pipeline.sh
@@ -6,13 +6,14 @@ websocket="showdown-server"
 service="showdown"
 port=8081
 baseip="172.25.0.0"
+teamdir="''"
 # Modes:
 # gen8customgame
 # gen8randombattle
 # gen8randombattle3  (not finished yet, need to switch showdown to newer commit)
 gamemode="gen8randombattle3"
 
-python3 pmariglia/envs/create_envs.py --websocket "$websocket:$port" --count $count --runcount 10 --gamemode "$gamemode@@@Dynamax Clause" --datadir dataset --battlebot most_damage --dest $dest 
+python3 pmariglia/envs/create_envs.py --websocket "$websocket:$port" --count $count --runcount 10 --gamemode "$gamemode@@@Dynamax Clause" --datadir dataset --battlebot most_damage --dest $dest --teamdir $teamdir
 
-python3 create_compose.py --count $count --port $port --servername $websocket --serverservice $service 
+python3 create_compose.py --count $count --port $port --servername $websocket --serverservice $service --baseip $baseip
 
