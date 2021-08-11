75853bc02129b81bb576f8568f693a90764749ba
Arman Mielke <arman.mielke@yahoo.de>
2021-08-11 19:41:29 +0200

    Feed PlayerData into the policy network

--- mcts/src/policy_network.cpp
+++ mcts/src/policy_network.cpp
@@ -2,14 +2,70 @@
 
 #include <torch/script.h>
 
+using namespace torch;
+
 
 // see https://pytorch.org/tutorials/advanced/cpp_export.html#step-3-loading-your-script-module-in-c
 PolicyNetwork::PolicyNetwork(std::string const model_path) {
     this->model = torch::jit::load(model_path);
 }
 
+Tensor convert_p1_pokemon_to_tensor(PokemonData const pokemon) {
+    return torch::cat({
+        torch::from_blob((int*)&pokemon.is_active, 1),
+        torch::from_blob((int*)&pokemon.hp, 1),
+        torch::from_blob((int*)pokemon.stats.data(), NUM_STATS),
+        torch::from_blob((int*)pokemon.types.data(), NUM_TYPES),
+        torch::from_blob((int*)pokemon.moves.data(), NUM_MOVES),
+        torch::from_blob((int*)pokemon.move_types[0].data(), NUM_TYPES),
+        torch::from_blob((int*)pokemon.move_types[1].data(), NUM_TYPES),
+        torch::from_blob((int*)pokemon.move_types[2].data(), NUM_TYPES),
+        torch::from_blob((int*)pokemon.move_types[3].data(), NUM_TYPES),
+        torch::from_blob((int*)pokemon.move_damages.data(), NUM_MOVES),
+        torch::from_blob((int*)pokemon.move_categories.data(), NUM_MOVES)
+    });
+}
+
+Tensor convert_p2_pokemon_to_tensor(PokemonData const pokemon) {
+    return torch::cat({
+        torch::from_blob((int*)&pokemon.is_active, 1),
+        torch::from_blob((int*)&pokemon.hp, 1),
+        torch::from_blob((int*)pokemon.stats.data(), NUM_STATS),
+        torch::from_blob((int*)pokemon.types.data(), NUM_TYPES),
+        torch::from_blob((int*)pokemon.move_types[0].data(), NUM_TYPES),
+        torch::from_blob((int*)pokemon.move_types[1].data(), NUM_TYPES),
+        torch::from_blob((int*)pokemon.move_types[2].data(), NUM_TYPES),
+        torch::from_blob((int*)pokemon.move_types[3].data(), NUM_TYPES)
+    });
+}
+
+std::array<float, 4> PolicyNetwork::evaluate_policy(PlayerData const p1, PlayerData const p2) {
+    // TODO make the number of Pokémon flexible
+    Tensor p1_tensor = torch::stack({
+        convert_p1_pokemon_to_tensor(p1[0]),
+        convert_p1_pokemon_to_tensor(p1[1]),
+        convert_p1_pokemon_to_tensor(p1[2]),
+    }, 0);
+    p1_tensor = torch::unsqueeze(p1_tensor, 0);
+    Tensor p2_tensor = torch::stack({
+        convert_p2_pokemon_to_tensor(p2[0]),
+        convert_p2_pokemon_to_tensor(p2[1]),
+        convert_p2_pokemon_to_tensor(p2[2]),
+    }, 0);
+    p2_tensor = torch::unsqueeze(p2_tensor, 0);
+
+    Tensor action_probabilities = this->model_forward(p1_tensor, p2_tensor);
+    action_probabilities = torch::squeeze(action_probabilities, 0);
+    return {
+        action_probabilities[0].item<float>(),
+        action_probabilities[1].item<float>(),
+        action_probabilities[4].item<float>(),
+        action_probabilities[5].item<float>()
+    };
+}
+
 // see https://pytorch.org/tutorials/advanced/cpp_export.html#step-4-executing-the-script-module-in-c
-torch::Tensor PolicyNetwork::forward(const torch::Tensor p1, const torch::Tensor p2) {
+torch::Tensor PolicyNetwork::model_forward(const torch::Tensor p1, const torch::Tensor p2) {
     // create a vector of inputs
     std::vector<torch::jit::IValue> inputs = {p1, p2};
     // execute the model and turn its output into a tensor
--- mcts/src/policy_network.h
+++ mcts/src/policy_network.h
@@ -1,6 +1,7 @@
 #ifndef POKEMON_MCTS_LOAD_MODEL_H
 #define POKEMON_MCTS_LOAD_MODEL_H
 
+#include "showdown_simulator.h"
 
 #include <string>
 
@@ -11,12 +12,20 @@ class PolicyNetwork {
 public:
     /// Loads the model from the given path.
     PolicyNetwork(std::string const model_path);
-    /// @param p1: Team of player 1. Shape (batch_size, num_pokemon, p1_pokemon_size)
-    /// @param p2: Team of player 2. Shape (batch_size, num_pokemon, p2_pokemon_size)
-    torch::Tensor forward(torch::Tensor const p1, torch::Tensor const p2);
+    /// Estimates which action should be taken given some information about the game state.
+    /// @param p1: Information about the player taking the action
+    /// @param p2: Information about the opponent
+    /// @return: Action probabilities
+    std::array<float, 4> evaluate_policy(PlayerData const p1, PlayerData const p2);
 
 private:
+    // TODO can this be const?
     torch::jit::script::Module model;
+
+    /// @param p1: Team of player 1. Shape (batch_size, num_pokemon, p1_pokemon_size)
+    /// @param p2: Team of player 2. Shape (batch_size, num_pokemon, p2_pokemon_size)
+    // TODO can this function be const?
+    torch::Tensor model_forward(torch::Tensor const p1, torch::Tensor const p2);
 };
 
 
