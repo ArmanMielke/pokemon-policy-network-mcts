b9614db4850e648d91554d9576dc7238efd614a0
Arman Mielke <arman.mielke@yahoo.de>
2021-08-16 20:05:24 +0200

    Add SimpleHeuristicPolicy

---
+++ mcts/src/simple_heuristic_policy.cpp
@@ -0,0 +1,84 @@
+#include "simple_heuristic_policy.h"
+#include "player_data.h"
+
+#include <torch/torch.h>
+
+using torch::Tensor;
+using torch::indexing::Slice;
+
+
+float const SWITCH_LOGIT = 60;
+Tensor const SWITCH_LOGITS = SWITCH_LOGIT * torch::ones(NUM_POKEMON - 1);
+
+std::array<float, NUM_TYPES * NUM_TYPES> const TYPE_EFFECTIVENESS_CHART = {
+    /*             Norm Figh Fly  Pois Grou Rock Bug  Ghos Stee Fire Wate Gras Elec Psyc Ice  Drag Dark Fairy */
+    /*   Normal */ 1  , 1  , 1  , 1  , 1  , 0.5, 1  , 0  , 0.5, 1  , 1  , 1  , 1  , 1  , 1  , 1  , 1  , 1  ,
+    /* Fighting */ 2  , 1  , 0.5, 0.5, 1  , 2  , 0.5, 0  , 2  , 1  , 1  , 1  , 1  , 0.5, 2  , 1  , 2  , 0.5,
+    /*   Flying */ 1  , 2  , 1  , 1  , 1  , 0.5, 2  , 1  , 0.5, 1  , 1  , 2  , 0.5, 1  , 1  , 1  , 1  , 1  ,
+    /*   Poison */ 1  , 1  , 1  , 0.5, 0.5, 0.5, 1  , 0.5, 0  , 1  , 1  , 2  , 1  , 1  , 1  , 1  , 1  , 2  ,
+    /*   Ground */ 1  , 1  , 0  , 2  , 1  , 2  , 0.5, 1  , 2  , 2  , 1  , 0.5, 2  , 1  , 1  , 1  , 1  , 1  ,
+    /*     Rock */ 1  , 0.5, 2  , 1  , 0.5, 1  , 2  , 1  , 0.5, 2  , 1  , 1  , 1  , 1  , 2  , 1  , 1  , 1  ,
+    /*      Bug */ 1  , 0.5, 0.5, 0.5, 1  , 1  , 1  , 0.5, 0.5, 0.5, 1  , 2  , 1  , 2  , 1  , 1  , 2  , 0.5,
+    /*    Ghost */ 0  , 1  , 1  , 1  , 1  , 1  , 1  , 2  , 1  , 1  , 1  , 1  , 1  , 2  , 1  , 1  , 0.5, 1  ,
+    /*    Steel */ 1  , 1  , 1  , 1  , 1  , 2  , 1  , 1  , 0.5, 0.5, 0.5, 1  , 0.5, 1  , 2  , 1  , 1  , 2  ,
+    /*     Fire */ 1  , 1  , 1  , 1  , 1  , 0.5, 2  , 1  , 2  , 0.5, 0.5, 2  , 1  , 1  , 2  , 0.5, 1  , 1  ,
+    /*    Water */ 1  , 1  , 1  , 1  , 2  , 2  , 1  , 1  , 1  , 2  , 0.5, 0.5, 1  , 1  , 1  , 0.5, 1  , 1  ,
+    /*    Grass */ 1  , 1  , 0.5, 0.5, 2  , 2  , 0.5, 1  , 0.5, 0.5, 2  , 0.5, 1  , 1  , 1  , 0.5, 1  , 1  ,
+    /* Electric */ 1  , 1  , 2  , 1  , 0  , 1  , 1  , 1  , 1  , 1  , 2  , 0.5, 0.5, 1  , 1  , 0.5, 1  , 1  ,
+    /*  Psychic */ 1  , 2  , 1  , 2  , 1  , 1  , 1  , 1  , 0.5, 1  , 1  , 1  , 1  , 0.5, 1  , 1  , 0  , 1  ,
+    /*      Ice */ 1  , 1  , 2  , 1  , 2  , 1  , 1  , 1  , 0.5, 0.5, 0.5, 2  , 1  , 1  , 0.5, 2  , 1  , 1  ,
+    /*   Dragon */ 1  , 1  , 1  , 1  , 1  , 1  , 1  , 1  , 0.5, 1  , 1  , 1  , 1  , 1  , 1  , 2  , 1  , 0  ,
+    /*     Dark */ 1  , 0.5, 1  , 1  , 1  , 1  , 1  , 2  , 1  , 1  , 1  , 1  , 1  , 2  , 1  , 1  , 0.5, 0.5,
+    /*    Fairy */ 1  , 2  , 1  , 0.5, 1  , 1  , 1  , 1  , 0.5, 0.5, 1  , 1  , 1  , 1  , 1  , 2  , 2  , 1  ,
+};
+Tensor const TYPE_EFFECTIVENESS_MATRIX = torch::from_blob((int*)TYPE_EFFECTIVENESS_CHART.data(), {NUM_TYPES, NUM_TYPES});
+
+
+std::array<float, 4> SimpleHeuristicPolicy::evaluate_policy(PlayerData const p1, PlayerData const p2) {
+    // the logits for attacks are the move's base damage times the STAB bonus times the type effectiveness modifier
+    // (assuming the opponent doesn't switch)
+    Tensor const base_damages = torch::from_blob((int*)p1[0].move_damages.data(), NUM_MOVES);
+
+    Tensor stab_bonus = torch::ones(NUM_MOVES);
+    Tensor type_effectiveness_modifier = torch::ones(NUM_MOVES);
+    Tensor const attacking_pokemon_type = torch::from_blob((int*)p1[0].types.data(), NUM_TYPES);
+    Tensor const defending_pokemon_type = torch::from_blob((int*)p2[0].types.data(), NUM_TYPES);
+    for (size_t move = 0; move < NUM_MOVES; move++) {
+        // stab bonus
+        Tensor const move_type = torch::from_blob((int*)p1[0].move_types[move].data(), NUM_TYPES);
+        //                         TODO use matmul?
+        int const has_stab_bonus = torch::sum(attacking_pokemon_type * move_type).item<int>();
+        if (has_stab_bonus == 1) {
+            stab_bonus[move] = 1.5;
+        }
+
+        // type effectiveness
+        Tensor move_effectiveness = matmul(move_type, TYPE_EFFECTIVENESS_MATRIX);
+        std::cout << move_effectiveness << std::endl;
+        type_effectiveness_modifier[move] = matmul(move_effectiveness, defending_pokemon_type);
+        std::cout << type_effectiveness_modifier[move] << std::endl;
+    }
+
+    std::cout << base_damages << std::endl;
+    std::cout << stab_bonus << std::endl;
+    std::cout << type_effectiveness_modifier << std::endl;
+    Tensor move_logits = base_damages * stab_bonus * type_effectiveness_modifier;
+    std::cout << move_logits << std::endl;
+
+    // we're currently only using two moves => throw away the logits of the other two moves
+    move_logits = move_logits.index({Slice(0, 1)});
+    std::cout << move_logits << std::endl;
+
+    Tensor const logits = torch::cat({move_logits, SWITCH_LOGITS}, 0);
+    std::cout << logits << std::endl;
+
+    Tensor const action_probabilities = torch::softmax(logits, 0);
+    std::cout << action_probabilities << std::endl;
+
+    return {
+            action_probabilities[0].item<float>(),
+            action_probabilities[1].item<float>(),
+            action_probabilities[2].item<float>(),
+            action_probabilities[3].item<float>()
+    };
+}
---
+++ mcts/src/simple_heuristic_policy.h
@@ -0,0 +1,18 @@
+#ifndef POKEMON_MCTS_SIMPLE_HEURISTIC_POLICY_H
+#define POKEMON_MCTS_SIMPLE_HEURISTIC_POLICY_H
+
+#include "player_data.h"
+
+
+/// Estimates action probabilities based on a simple, hand-crafted heuristic.
+class SimpleHeuristicPolicy {
+public:
+    /// Estimates which action should be taken given some information about the game state.
+    /// @param p1: Information about the player taking the action
+    /// @param p2: Information about the opponent
+    /// @return: Action probabilities
+    std::array<float, 4> evaluate_policy(PlayerData const p1, PlayerData const p2);
+};
+
+
+#endif //POKEMON_MCTS_SIMPLE_HEURISTIC_POLICY_H
