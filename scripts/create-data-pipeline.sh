017bed596ded97a4f76adaf1e69fc3f52b8fad76
Daniel Derkacz <mail@daniel-derkacz.de>
2021-08-18 14:12:56 +0200

    moved creation scripts to scripts folder

--- scripts/create-data-pipeline.sh
+++ scripts/create-data-pipeline.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+SCRIPT_DIR=`dirname "$0"`
+BASE_DIR=$SCRIPT_DIR/..
 
 #--------------------------------
 # Important to change
@@ -14,24 +16,24 @@ TEAM_DIR="switch_three_pokemon"
 # Not that important to change
 BATTLE_BOT="most_damage"
 ENV_DEST="pmariglia/envs"
-COUNT=16
+COUNT=1
 RUN_COUNT=10
 BASE_IP="172.25.0.0"
 SERVER_SERVICE="showdown"
-POSTFIX="-2"
+POSTFIX="-1"
 COMPOSE_TYPE="data"
-FILE_DEST="docker-compose.yml"
+FILE_DEST="$BASE_DIR/docker-compose.yml"
 DATA_DIR="dataset"
 #-------------------------------
 
 
 # Create the pmariglia environment files
-python3 pmariglia/envs/create_envs.py --websocket "'$WEBSOCKET:$PORT'" \
+python3 $BASE_DIR/pmariglia/envs/create_envs.py --websocket "'$WEBSOCKET:$PORT'" \
     --count $COUNT --runcount $RUN_COUNT --gamemode "$GAME_MODE" \
     --datadir $DATA_DIR --teamdir $TEAM_DIR --battlebot $BATTLE_BOT \
     --dest $ENV_DEST
 
 # create the docker compose file
-python3 create_compose.py --count $COUNT --port $PORT --baseip $BASE_IP \
+python3 $SCRIPT_DIR/create_compose.py --count $COUNT --port $PORT --baseip $BASE_IP \
     --servername $WEBSOCKET --serverservice $SERVER_SERVICE \
     --postfix $POSTFIX --kind $COMPOSE_TYPE --dest $FILE_DEST
--- scripts/create-mcts-pipeline.sh
+++ scripts/create-mcts-pipeline.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+SCRIPT_DIR=`dirname "$0"`
+BASE_DIR=$SCRIPT_DIR/..
 
 #--------------------------------
 # Important to change
@@ -13,15 +15,15 @@ TEAM_DIR_MCTS="/mcts/teams/switch_three_pokemon_packed"
 
 #-------------------------------
 # Not that important to change
-BATTLE_BOT="most_base_damage"
+BATTLE_BOT="random"
 ENV_DEST="pmariglia/envs"
-COUNT=10
+COUNT=1
 RUN_COUNT=2
 BASE_IP="172.25.0.0"
 SERVER_SERVICE="showdown-mcts"
 POSTFIX="-1"
 COMPOSE_TYPE="mcts"
-FILE_DEST="mcts-docker-compose.yml"
+FILE_DEST="$BASE_DIR/mcts-docker-compose.yml"
 DATA_DIR="dataset"
 TIMER="False"
 ROLLOUTS=100
@@ -29,12 +31,12 @@ ROLLOUT_LENGTH=100
 AGENT="MCTS_VANILLA"
 #-------------------------------
 
-python3 pmariglia/envs/create_envs.py --websocket "$WEBSOCKET:$PORT" \
+python3 $BASE_DIR/pmariglia/envs/create_envs.py --websocket "$WEBSOCKET:$PORT" \
     --count $COUNT --runcount $RUN_COUNT --gamemode "$GAME_MODE" \
     --datadir $DATA_DIR --teamdir $TEAM_DIR --battlebot $BATTLE_BOT \
     --dest $ENV_DEST --timer $TIMER
 
-python3 create_compose.py --count $COUNT --port $PORT \
+python3 $SCRIPT_DIR/create_compose.py --count $COUNT --port $PORT \
     --baseip $BASE_IP --servername $WEBSOCKET --serverservice $SERVER_SERVICE \
     --postfix $POSTFIX --kind $COMPOSE_TYPE --dest $FILE_DEST \
     --gameformat "$GAME_MODE" --teamdir $TEAM_DIR_MCTS --numbattles $RUN_COUNT \
